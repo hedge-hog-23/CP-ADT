77c08ee48a69b36a5b42f156db049287ee355c3f
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2023-07-17 15:57:05 +0530

    Create 2pointer.c

---
+++ LAB -02/2pointer.c
@@ -0,0 +1,76 @@
+// Find the intersection of two sorted arrays.
+
+// OR in other words,
+
+// Given 2 sorted arrays, find all the elements which occur in both the arrays. 
+
+// Input Format
+
+// ·       The first line contains T, the number of test cases. Following T lines contain:
+
+// 1.     Line 1 contains N1, followed by N1 integers of the first array
+
+// 2.     Line 2 contains N2, followed by N2 integers of the second array
+
+// Output Format
+
+// The intersection of the arrays in a single line
+
+// Example
+
+// Input:
+
+// 1
+
+// 3 10 17 57
+
+// 6 2 7 10 15 57 246
+
+// Output:
+
+// 10 57
+
+// Input:
+
+// 1
+
+// 6 1 2 3 4 5 6
+
+// 2 1 6
+
+// Output:
+
+// 1 6
+
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d ",&t);
+    for(int i=0;i<t;i++){
+        int n,m;
+        scanf("%d ",&n);
+        int a[n];
+        for(int i=0;i<n;i++){
+            scanf("%d ",&a[i]);
+        }
+        scanf("%d ",&m);
+        int b[m];
+        for(int i=0;i<m;i++){
+            scanf("%d ",&b[i]);
+        }
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(a[i]<b[j])
+                i++;
+            else if(a[i] == b[j]){
+                printf("%d ",a[i]);
+                i++;
+                j++;
+            }
+            else
+                j++;
+                
+        }
+    }
+}
